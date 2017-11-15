a4fcf964e3c48d60357815afeaa8d76213ee6a09
StephenPhan <30671875+StephenPhan@users.noreply.github.com>
2017-11-15 14:50:49 -0800

    Update pleasework.py

--- pleasework.py
+++ pleasework.py
@@ -1,6 +1,14 @@
+### Hi all, Stephen here. Will be adding changes incrementally. Just hmu with questions.
+
+### CHANGELOG
+### v0.1.1 (11 15 17): 
+### removed need for LinkedIn_index.csv
+### added datetime in output file name
+
 import time
 import re
 import csv
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
@@ -8,6 +16,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 def depart():
+    '''a function that closes the "Function" pill-box to allow something else to be inputted'''
     try:
         driver.find_element_by_xpath('//*[@id="pagekey-sales-search3-people"]/section/div[2]/div/p/div/section[2]/ul/li[1]/div/ul/li[1]/label').click()
     except Exception:
@@ -20,55 +29,44 @@ def depart():
     time.sleep(1)
     return
 
-companies = []
-remainder = []
-CSVOUTPUT = []
+#declarations
+remainder = [] #companyIDs from .csv
+CSVOUTPUT = [] #output
+driver = webdriver.Firefox() #browser
+actions = ActionChains(driver) #selenium queue
+name = datetime.today().strftime("%Y%m%d %H%M") #for output name
+prefix = 'https://www.linkedin.com/sales/search?facet=CC&facet.CC=' #prefix for URL
+catget = ['Engineering', 'Arts and Design'] #search terms
+#catget = ['Quality Assurance']
 
-###
-#companypage = 'https://www.linkedin.com/sales/accounts/insights?companyId='
-###
+#.csv dialect
+csv.register_dialect('mydialect',delimiter='|',quotechar='"',doublequote=True,
+skipinitialspace=True,lineterminator='\r\n',quoting=csv.QUOTE_MINIMAL)
 
-csv.register_dialect('mydialect',delimiter='|',quotechar='"',doublequote=True,skipinitialspace=True,lineterminator='\r\n',quoting=csv.QUOTE_MINIMAL)
-
+#open .csv with companyIDs
 with open('LinkedIn_get.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         remainder.append(row[0])
 
-with open('Linkedin_index.csv') as q:
-    reader = csv.reader(q)
-    for row in reader:
-        companies.append(row[0])
-
-driver = webdriver.Firefox()
-prefix = 'https://www.linkedin.com/sales/search?facet=CC&facet.CC='
-catget = ['Engineering', 'Arts and Design']
-#catget = ['Quality Assurance']
-
 #sign-in
 driver.get('https://www.linkedin.com/sales/search/companies')
 email = driver.find_element_by_xpath('//*[@id="session_key-login"]')
-email.send_keys('utestacco@gmail.com')
 pw = driver.find_element_by_xpath('//*[@id="session_password-login"]')
+email.send_keys('utestacco@gmail.com')
 pw.send_keys('unitytest')
 driver.find_element_by_xpath('//*[@id="btn-primary"]').click()
 
-actions = ActionChains(driver)
-
 time.sleep(3)
 
-for companyID, companyName in zip(remainder, companies):
+#scrape get
+for companyID in remainder:
     driver.get(prefix+str(companyID))
-    # for cat in catget:
     time.sleep(3)
-    # companyname = driver.find_element_by_xpath('//*[@id="pivot-banner"]/div/div[1]/div/h1/a').text
-    # print(companyname+' ('+companyID+')'+': '+employeecount)
     driver.find_element_by_class_name('view-all-filters').click()
     time.sleep(2)
     employeecount = driver.find_element_by_class_name('result-count').text
-    #print(employeecount)
     block = driver.find_element_by_xpath('//*[@id="pagekey-sales-search3-people"]/section')
-    # filtersearch = block.find_element_by_class_name('top-facets')
     filterblock = block.find_element_by_xpath('//*[@id="pagekey-sales-search3-people"]/section/div[2]/div/p/div/section[1]/ul/li[7]')
     time.sleep(3)
     try:
@@ -76,63 +74,43 @@ for companyID, companyName in zip(remainder, companies):
     except Exception:
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="pagekey-sales-search3-people"]/section/div[2]/div/p/div/section[1]/ul/li[7]/div/div[1]/span[1]').click()
-    #
     
     time.sleep(1)
-    
-    #filterthing = filterblock.find_element_by_xpath('//*[@id="FA-input"]')
     filterthing = driver.find_elements_by_class_name('add-values-container')
-    
 
     i = 0
     for string in filterthing:
         if string.text == 'Add types of roles':
             for cat in catget:
-                if i == 0:
+                if i == 0: #searching first catget term
                     string.send_keys(cat)
                     time.sleep(2)
                     string.send_keys('\ue007')
                     time.sleep(1)
-                    #Engineering
-                else:
+                else: #searching succeeding catget term(s)
                     depart()
-                    #print('Category--',cat)
                     actions.send_keys(cat)
-                    '''actions.perform()
-                    actions.reset_actions()'''
                     time.sleep(1)
                     actions.send_keys('\ue007')
                     actions.perform()
                     time.sleep(1)
                     actions.reset_actions()
-                    #the other ones, QA + arts and design
-                    
-                    #print(driver.find_element_by_xpath('//*[@id="pagekey-sales-search3-people"]/section/div[2]/div/p/div/section[2]/ul/li[1]/div/ul/li[1]/label/span').text)
-                    '''if i == 2:
-                        depart()
-                        print('Departing..!')
-                        actions.perform()'''
+
                     time.sleep(2)
-                    
                     actions.reset_actions()
                     
                 if i == 0:
                     engineering = driver.find_element_by_class_name('result-count').text
-                    #print(cat+': '+engineering)
                     time.sleep(2)
                 '''if i == 0:
                     qualityassurance = driver.find_element_by_class_name('result-count').text
-                    #print(cat+': '+qualityassurance)
-                    
                     time.sleep(2)'''
                 if i == 1:
-                    designers = driver.find_element_by_class_name('result-count').text
-                    #print(cat+': '+designers)                       
+                    designers = driver.find_element_by_class_name('result-count').text                 
                     time.sleep(2)
                 i += 1
                 if i >= 2:
                     break
-            #print('Loop done.')
             break
     try:
         print(companyID, '|', employeecount, '|', engineering, '|', designers)
@@ -142,7 +120,7 @@ for companyID, companyName in zip(remainder, companies):
     except Exception:
         pass
 
-with open('auto_oem_linkedin3.csv', 'w', encoding='utf8') as mycsvfile:
+with open('LinkedIn_out %s.csv' %name, 'w', encoding='utf8') as mycsvfile:
     DATAWRITE = csv.writer(mycsvfile, dialect='mydialect')
     for row in CSVOUTPUT:
         DATAWRITE.writerow(row)
